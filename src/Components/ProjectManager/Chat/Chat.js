b1f6f36649564b7b8c57bbf3d716b2c4e5a51ab2
Denis Gichana <denisgichana162@gmail.com>
2022-10-16 20:44:38 +0300

    WIP

---
+++ src/Components/ProjectManager/Chat/Chat.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Chat() {
+  return (
+    <div className="left__chat">
+      <div className="message">
+        <p>hello Dennis</p>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
--- src/Components/ProjectManager/ProjectManager.js
+++ src/Components/ProjectManager/ProjectManager.js
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getProject } from "../../redux/actions/projectActions";
 import "./style.css";
+import Chat from "./Chat/Chat";
 
 function ProjectManager() {
   const { projectName } = useParams();
@@ -20,24 +21,23 @@ function ProjectManager() {
       <div className="project__nav">
         <div className="project__nav__button">
           <GiFiles />
-          <p>
-            Files
-            </p>
+          <p>Files</p>
         </div>
         <div className="project__nav__button">
           <FaTasks />
-          <p>
-            Tasks
-            </p>
+          <p>Tasks</p>
         </div>
         <div className="project__nav__button">
           <BiTimeFive />
-          <p>
-            Timeline
-            </p>
+          <p>Timeline</p>
         </div>
       </div>
-      {project && <div>{project.date}</div>}
+      <div className="project__manager__body">
+        {project && <div>{project.date}</div>}
+        <div className="chat">
+          <Chat />
+        </div>
+      </div>
     </div>
   );
 }

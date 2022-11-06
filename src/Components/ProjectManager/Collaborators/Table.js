da314baa8f41a830d1e7c87c4cfd97afea69cd2c
Denis Gichana <denisgichana162@gmail.com>
2022-11-06 13:03:56 +0300

    WIP

--- src/Components/ProjectManager/Collaborators/Table.js
+++ src/Components/ProjectManager/Collaborators/Table.js
@@ -1,32 +1,52 @@
 import axios from "axios";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { saveNotifications } from "../../../redux/actions/notificationActions";
 import "./style.css";
-const Table = ({ data }) => {
+const Table = ({ data, setInvite }) => {
   const { project } = useSelector((state) => state.projectReducer);
-  const inviteUser = (name) => {
+  const { user } = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+  const inviteUser = (item) => {
     axios.post("http://localhost:5000/notification", project.projectName);
+    const notification = {
+      projectName: project.projectName,
+      link: `${user.name}/${project.projectName}`,
+      receiver: item.name,
+      sender: user.name,
+    };
+    dispatch(saveNotifications(notification));
+    // add a .then method to dispatch
+    const invite = item;
+    setInvite((prevInvite) => ({ ...prevInvite, invite }));
   };
   return (
-    <table>
-      <tbody>
-        <tr>
-          <th>pic</th>
-          <th>username</th>
-          <th>Invite</th>
-        </tr>
-        {data?.map((item) => (
-          <tr key={item.id}>
-            <td>
-              <img src={item.profilePic} />{" "}
-            </td>
-            <td>{item.name}</td>
-            <td>
-              <button onClick={inviteUser(item.name)}>Invite</button>
-            </td>
+    <div className="table">
+      <table>
+        <tbody>
+          <tr calssName="table-header">
+            <th>pic</th>
+            <th>username</th>
+            <th>Invite</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+          {data?.map((item) => (
+            <tr key={item.id} className="table-row">
+              <td>
+                <img
+                  className="table__image"
+                  src={`${window.location.origin}/images/profiles/${item.profilePic}`}
+                />
+              </td>
+              <td>{item.name}</td>
+              <td>
+                <button className="table__invite" onClick={inviteUser(item)}>
+                  Invite
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 

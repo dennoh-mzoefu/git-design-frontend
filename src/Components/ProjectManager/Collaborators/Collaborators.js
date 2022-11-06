8502f6d751a4bf1413bbc234ff8a5fec4677c5d5
Denis Gichana <denisgichana162@gmail.com>
2022-11-06 13:04:08 +0300

    WIP

--- src/Components/ProjectManager/Collaborators/Collaborators.js
+++ src/Components/ProjectManager/Collaborators/Collaborators.js
@@ -5,41 +5,52 @@ import Table from "./Table";
 
 function Collaborators() {
   const [query, setQuery] = useState("");
+  const [invite, setInvite] = useState("");
+
+  const { notifications } = useSelector((state) => state.notificationReducer);
+  // filter receiver-notifications and remove them from variable users below to a new array and display the array
+  // const receiver =
+  // const a = notifications?.map(b => {
+
+  // })
   const { users } = useSelector((state) => state.userReducer);
-  // const Users = [
-  //   {
-  //     id: 1,
-  //     first_name: "Emiline",
-  //     last_name: "McClune",
-  //     email: "emcclune0@xrea.com",
-  //     gender: "Female",
-  //   },
-  //   {
-  //     id: 2,
-  //     first_name: "Felix",
-  //     last_name: "Ingleston",
-  //     email: "fingleston1@hibu.com",
-  //     gender: "Female",
-  //   },
-  // ];
+
   console.log(users);
   const Search = (data) => {
     return data?.filter((item) => item.name.includes(query));
   };
   const handleSubmit = () => {};
   return (
-    <div className="search-container">
-      <form onSubmit={handleSubmit}>
-        <input
-          className="search-input"
-          placeholder="Search..."
-          onChange={(e) => setQuery(e.target.value.toLowerCase())}
-        />
-        <a href="" onClick={handleSubmit} className="search-btn">
-          <i className="fas fa-search"></i>
-        </a>
-      </form>
-      {users && <Table data={Search(users)} />}
+    <div className="collaborators">
+      <div className="collab">
+        <div className="search-container">
+          <form onSubmit={handleSubmit}>
+            <input
+              className="search-input"
+              placeholder="Search..."
+              onChange={(e) => setQuery(e.target.value.toLowerCase())}
+            />
+            <a href="" onClick={handleSubmit} className="search-btn">
+              <i className="fas fa-search"></i>
+            </a>
+          </form>
+        </div>
+        <div className="users">
+          {users && <Table data={Search(users)} setInvite={setInvite} />}
+        </div>
+      </div>
+      <div className="invite__list">
+        Invited
+        {invite?.map((item) => {
+          <div className="invite__card__image">
+            <img
+              className="table__image"
+              src={`${window.location.origin}/images/profiles/${item.profilePic}`}
+            />
+          </div>;
+        })}
+        {/* </div> */}
+      </div>
     </div>
   );
 }
